0cde32330e3467975629a2a0cc5be559cdbd51d8
MohamedElatroush <muhamed_hatem1@aucegypt.edu>
2023-12-07 00:49:52 +0200

    fix cors

--- dashboardAPI/settings.py
+++ dashboardAPI/settings.py
@@ -190,5 +190,6 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://54.145.211.86/",
     "http://noce-dashboard2.s3-website-us-east-1.amazonaws.com/"
+    
     # Add other domains as needed
 ]
