da458e20be22181c6a9ced1e73666a4349422dd1
MohamedElatroush <muhamed_hatem1@aucegypt.edu>
2024-02-11 22:16:11 +0200

    Fixes v4.4.5

--- dashboard/jobs/updater.py
+++ dashboard/jobs/updater.py
@@ -33,8 +33,8 @@ def start():
         scheduler.add_job(generate_noce_timesheet, 'cron', hour=13, minute=0)
         scheduler.add_job(generate_ts_ocg, 'cron', hour=13, minute=5, args=('param_ocg',), id='ts_ocg')
         scheduler.add_job(generate_ts_nk, 'cron', hour=13, minute=10, args=('param_nk',), id='ts_nk')
-        scheduler.add_job(generate_ts_EHAF, 'cron', hour=22, minute=15, args=('param_EHAF',), id='ts_EHAF')
-        scheduler.add_job(generate_ts_ACE, 'cron', hour=22, minute=8, args=('param_ACE',), id='ts_ACE')
+        scheduler.add_job(generate_ts_EHAF, 'cron', hour=13, minute=15, args=('param_EHAF',), id='ts_EHAF')
+        scheduler.add_job(generate_ts_ACE, 'cron', hour=13, minute=20, args=('param_ACE',), id='ts_ACE')
 
         scheduler.start()
     except Exception as e:
